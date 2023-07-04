c099b159ac295f72bafd734ef951780995d78785
adairconlin <adair.conlin@gmail.com>
2023-07-04 10:34:12 -0400

    Users are create when given points

--- utility/index.js
+++ utility/index.js
@@ -1,4 +1,4 @@
 const { getUserPoints, setUserPoints, giveUserPoints, takeUserPoints } = require("./points-util");
-const { checkForScanChannels, checkForCloneChannels, checkForAutoPointChannels } = require("./database-util");
+const { checkForScanChannels, checkForCloneChannels, checkForAutoPointChannels } = require("./event-util");
 
 module.exports = { getUserPoints, setUserPoints, giveUserPoints, takeUserPoints, checkForScanChannels, checkForCloneChannels, checkForAutoPointChannels };
--- utility/points-util.js
+++ utility/points-util.js
@@ -1,14 +1,15 @@
 // Commands for interacting with points in the database
 const { User } = require("../schemas");
+const { addToDatabase } = require("./create-user");
 let value;
 
 const getUserPoints = async id => {
     const findUser = await User.find({ discordId: id });
     if(!findUser.length) {
-        return "This user is not registered.";
+        return "User not found."; // Valid scenario
     }
 
-    let arr = [findUser[0]?.pointsAvail, findUser[0]?.pointsAmt];
+    let arr = [ findUser[0]?.pointsAvail, findUser[0]?.pointsAmt ];
 
     return arr;
 }
@@ -17,11 +18,12 @@ const setUserPoints = async (id, int) => {
     if(!int || !id) {
         return "Please define both fields for this command.";
     } else if(int < 0) {
-        return "Please define a number of points higher than 0";
+        return "Please define a number of points higher than 0.";
     }
+
     const findUser = await User.find({ discordId: id });
     if(!findUser.length) {
-        return "This user is not registered.";
+        return "User not found."; //add to database
     }
 
     await User.updateOne({ discordId: id },
@@ -35,19 +37,28 @@ const setUserPoints = async (id, int) => {
     return value;
 }
 
-const giveUserPoints = async (id, int) => {
-    if(!id || !int) {
+/*
+* Return true upon successful update
+* Return false upon failure to update specified user
+* Return string of error message if missing a parameter or if
+* there was an error adding a new user to database
+*/
+const giveUserPoints = async (interaction, user, int) => {
+    if(!user || !int) {
         return "Please define both fields for this command.";
     } else if(int < 1) {
-        return "Please define a number of points higher than 0";
+        return "Please define a number of points higher than 0.";
     }
 
-    const findUser = await User.find({ discordId: id });
-    if(!findUser.length) {
-        return "This user is not registered.";
+    const findUser = addToDatabase(interaction, user, int); //verify
+
+    if(!findUser) {
+        return "Error in finding or adding user to database. Yell a sappy."
     }
 
-    await User.updateOne({ discordId: id },
+    console.log("findUser:");
+    console.log(findUser); //temp
+    await User.updateOne({ findUser }, //verify
         {
             $inc: {
                 pointsAmt: int,
@@ -65,12 +76,12 @@ const takeUserPoints = async (id, int) => {
     if(!id || !int) {
         return "Please define both fields for this command.";
     } else if(int < 1) {
-        return "Please define a number of points higher than 0";
+        return "Please define a number of points higher than 0.";
     }
 
     const findUser = await User.find({ discordId: id });
     if(!findUser.length) {
-        return "This user is not registered.";
+        return "This user is not registered."; //add to database
     }
 
     if(findUser?.pointsAvail - int < 0) {
