1cda5fab3cc4bea9a92eaafeacc1c5769c37d6e2
Adair Conlin <adairconlin@gmail.com>
2023-12-27 12:33:23 -0500

    Add comments

--- database-controllers/scan-controllers.js
+++ database-controllers/scan-controllers.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 
 //For checking and setting channels
 const scanChannelCheck = async newId => {
-    //await ScanChannel.deleteMany({});
-
+    // Find scanChannel schema for current environment (1 = prod discord, 2 = dev discord)
     const result = await ScanChannel.find({ id: process.env.ENV_ID });
 
     if(!result.length) {
@@ -15,6 +14,7 @@ const scanChannelCheck = async newId => {
 
         await new ScanChannel(newScan).save();
     } else {
+        // If this environment has a ScanChannel schema, check if this channel needs to be added
         checkForChannel(newId, result);
     }
 }
@@ -37,6 +37,5 @@ const addScanChannel = async newId => {
         }
     )
 }
-
 
 module.exports = { scanChannelCheck }
